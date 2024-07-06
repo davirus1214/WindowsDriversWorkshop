a3a72dac81f3f349f2a0986e22dd0fca1023da63
JuanVFletes <ldfozamis@estudiantec.cr>
2024-07-05 18:39:45 -0600

    Translated comments from Spanish to English in driver code KmdfHelloWorld

--- Samples/week-2/KmdfHelloWorld/Driver.c
+++ Samples/week-2/KmdfHelloWorld/Driver.c
@@ -1,70 +1,65 @@
-//HEaders necesarios
-#include <ntddk.h> // Contiene definiciones de todos los drivers del core Windows Kernel. como por ej DRIVER_INITIALIZE
-#include <wdf.h> // Contiene definiciones de drivers del Windows Driver Framework
+//Necessary Headers
+#include <ntddk.h> // Contains definitions for all core Windows Kernel drivers, such as DRIVER_INITIALIZE
+#include <wdf.h> // Contains definitions for Windows Driver Framework drivers
 
-// Es este ejemplo se utilizarán 2 callbacks: el primero es DriverEntry, el segundo es KmdfHelloWorldEvtDeviceAdd
-// - DriverEntry: Punto de entrada
-// - KmdfHelloWorldEvtDeviceAdd: Se activaria cuando detecta un dispositivo.
+// In this example, 2 callbacks will be used: the first is DriverEntry, the second is KmdfHelloWorldEvtDeviceAdd
+// - DriverEntry: Entry point
+// - KmdfHelloWorldEvtDeviceAdd: Triggered when a device is detected.
 
-// Declaraciones de los callbacks
+// Callback declarations
 DRIVER_INITIALIZE DriverEntry;
-EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd; //es el punto de entrada para el driver cuando se agrega un dispositivo. Aqui el PnP (Plug and Play) manager reposta sobre la existencia de un dispositivo
+EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd; // This is the entry point for the driver when a device is added. Here the PnP (Plug and Play) manager reports the existence of a device
 
-// Vamos a empezar escribiendo el código del DriverEntry
-NTSTATUS   // tipo de dato, que lo utilizamos para devolver el estado de una operación (exitosa, error, ...)
-DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) { //_In_ Esto es para indicar que el parámetro es de entrada, o sea, la funcion no debería modificar el valor, solamente lo lee
-	// status sera la variable para indicar el estado si fue exitoso o fallido
-	NTSTATUS status = STATUS_SUCCESS;
+// We will start by writing the DriverEntry code
+NTSTATUS   // Data type used to return the status of an operation (successful, error, ...)
+DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) { //_In_ This indicates that the parameter is input, meaning the function should not modify the value, only read it
+    // status will be the variable to indicate whether the operation was successful or failed
+    NTSTATUS status = STATUS_SUCCESS;
 
-	// Driver Configuration Object, el cual nos sirve para cuando utilicemos más adelante EvtDeviceAdd 
-	WDF_DRIVER_CONFIG config;
+    // Driver Configuration Object, which will be used later when we utilize EvtDeviceAdd
+    WDF_DRIVER_CONFIG config;
 
-	// Directamente desde Driver Entry, cuando todo se inicializa, debe imprimir KmdfHelloWorld: DriverEntry
-	// Imprimiria un mensaje en la consola de depuracion. 
-	// DPFLTR_IHVDRIVER_ID: Este es un identificador de filtro que indica el origen del mensaje de depuración
-	// DPFLTR_INFO_LEVEL: indica que el mensaje es de nivel informativo.
-	// Mensaje: texto a mostrar
-	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
+    // Directly from Driver Entry, when everything is initialized, it should print KmdfHelloWorld: DriverEntry
+    // It would print a message to the debug console.
+    // DPFLTR_IHVDRIVER_ID: This is a filter identifier indicating the source of the debug message
+    // DPFLTR_INFO_LEVEL: Indicates that the message is informational level.
+    // Message: Text to display
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
 
-	// Inicializamos el driver configuration object para registrar el punto de entrada para la devolución de la llamada o el EvtDeviceAdd callback: KmdfHelloWorldEvtDeviceAdd
-	// parametro1: puntero al objeto de configuracion del Driver. 
-	// parametro2: es el punto de entrada para la devolución de llamada EvtDeviceAdd 
-	WDF_DRIVER_CONFIG_INIT(&config, KmdfHelloWorldEvtDeviceAdd); // Macro WDF_DRIVER_CONFIG_INIT
+    // Initialize the driver configuration object to register the entry point for the callback or the EvtDeviceAdd callback: KmdfHelloWorldEvtDeviceAdd
+    // parameter1: Pointer to the driver configuration object.
+    // parameter2: Entry point for the EvtDeviceAdd callback
+    WDF_DRIVER_CONFIG_INIT(&config, KmdfHelloWorldEvtDeviceAdd); // WDF_DRIVER_CONFIG_INIT Macro
 
-
-	// Crea el Driver Object
-	status = WdfDriverCreate(DriverObject, // puntero al driver object
-							 RegistryPath, // este puntero es la ubicacion en el registro de windows donde se almacenan los datos de configuracion específicos de este driver
-							 WDF_NO_OBJECT_ATTRIBUTES, // es para indicar que no se estan utilizando atributos adicionales
-							 &config, // puntero al objeto de configuracion que se inicializó en la linea 32
-							 WDF_NO_HANDLE // Opcional, y le indicamos que no requiere ningún id de entorno adicional.
-	);
-	return status;
-
-
+    // Create the Driver Object
+    status = WdfDriverCreate(DriverObject, // Pointer to the driver object
+                             RegistryPath, // This pointer is the location in the Windows registry where the configuration data specific to this driver is stored
+                             WDF_NO_OBJECT_ATTRIBUTES, // Indicates that no additional attributes are being used
+                             &config, // Pointer to the configuration object initialized in line 32
+                             WDF_NO_HANDLE // Optional, indicating that no additional environment handle is required
+    );
+    return status;
 }
 
-
-// el callback debe retornar un STATUS ( STATUS_SUCCESS, Error status value....)
+// The callback should return a STATUS (STATUS_SUCCESS, error status value...)
 NTSTATUS
-KmdfHelloWorldEvtDeviceAdd(_In_ WDFDRIVER Driver, _Inout_ PWDFDEVICE_INIT DeviceInit) { // _Inout_ es para indicar que el parametro es de entrada y salida, entonces la funcion puede leer y modificar.
-	// No estariamos utilizando el DriverObject llamado Driver entonces hay que colocarlo como UNREFERENCED_PARAMETER para que no de error
-	UNREFERENCED_PARAMETER(Driver);
+KmdfHelloWorldEvtDeviceAdd(_In_ WDFDRIVER Driver, _Inout_ PWDFDEVICE_INIT DeviceInit) { // _Inout_ indicates that the parameter is input and output, so the function can read and modify it.
+    // We are not using the DriverObject called Driver, so it must be marked as UNREFERENCED_PARAMETER to avoid an error
+    UNREFERENCED_PARAMETER(Driver);
 
-	// status sera la variable para indicar el estado si fue exitoso o fallido
-	NTSTATUS status;
+    // status will be the variable to indicate whether the operation was successful or failed
+    NTSTATUS status;
 
-	// esta variable representará el device object
-	WDFDEVICE hDevice;
+    // This variable will represent the device object
+    WDFDEVICE hDevice;
 
-	//Ahora volvemos a imprimir HelloWord pero cuando se detecta un dispositivo, por eso moestraremos este otro mensaje: "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"
-	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"));
+    // Now we print HelloWorld again but when a device is detected, so we will show this other message: "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"));
 
-	// Crea el Device Object
-	status = WdfDeviceCreate(&DeviceInit,
-							 WDF_NO_OBJECT_ATTRIBUTES,
-							 &hDevice // puntero al la variable que representa el deviceobject
-							);
-	return status;
-
+    // Create the Device Object
+    status = WdfDeviceCreate(&DeviceInit,
+                             WDF_NO_OBJECT_ATTRIBUTES,
+                             &hDevice // Pointer to the variable representing the device object
+                            );
+    return status;
 }
